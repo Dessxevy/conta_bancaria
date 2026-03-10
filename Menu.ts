c728cef8ce0f83166f9b5bcdb9ae8ee185070095
Andressa Andrade <andressaand0107@gmail.com>
2026-03-10 12:05:13 -0300

    feat: implementação da Classe Conta

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,17 @@
 import readlinesync = require("readline-sync");
 import { colors } from "./scr/util/Colors";
+import { Conta } from "./scr/model/Conta";
 
 export function main() {
 
     let opcao: number;
+
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(500);
+    conta.visualizar();
 
     while (true) {
 
